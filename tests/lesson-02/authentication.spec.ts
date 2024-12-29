96ceb41af222224c43f43d06bcaa03427394dae3
Minh Thu <minhthupham0907@gmail.com>
2024-12-29 12:00:51 +0700

    Update file Authentication

--- tests/lesson-02/authentication.spec.ts
+++ tests/lesson-02/authentication.spec.ts
@@ -1,63 +1,73 @@
 import { expect, test } from '@playwright/test';
 
 let validAccount = {
-    username: "1107-thu",
-    password: "HV%MhRjgyljzRDnxZCB(^Wl&"
+    username: '1107-thu',
+    password: 'HV%MhRjgyljzRDnxZCB(^Wl&',
 };
 
 let invalidAccount = {
-    username: "abc",
-    password: "123"
+    username: 'abc',
+    password: '123',
 };
 
-const loginPageURL = "https://pw-practice-dev.playwrightvn.com/wp-admin";
-const loginErrorMessage = "is not registered on this site";
+const loginPageURL = 'https://pw-practice-dev.playwrightvn.com/wp-admin';
+const loginErrorMessage = 'is not registered on this site';
 
-test.describe("AUTH-Authentication", async () => {
+let xpathUserName = '//input[@id="user_login"]';
+let xpathPassword = '//input[@id="user_pass"]';
+let xpathBtnSubmit = '//input[@id="wp-submit"]';
+let xpathLoginError = '//div[@id="login_error"]';
+
+test.describe('AUTH-Authentication', async () => {
     test.beforeEach(async ({ page }) => {
-        await page.goto(loginPageURL)
+        await page.goto(loginPageURL);
     });
 
-
-    test("@AUTH_001: Log fail", async ({ page }) => {
-        await test.step("Nhập vào thông tin username, password bị sai", async () => {
-            //Nhập username, password 
-            await page.locator('//input[@id="user_login"]').fill(invalidAccount.username);
-            await page.locator('//input[@id="user_pass"]').fill(invalidAccount.password);
+    test('@AUTH_001: Login fail', async ({ page }) => {
+        await test.step('Nhập vào thông tin username, password bị sai', async () => {
+            await page.locator(xpathUserName).fill(invalidAccount.username);
+            await page.locator(xpathPassword).fill(invalidAccount.password);
 
             //Kiểm tra giá trị của username password đã nhập
-            const actualUsername = await page.locator('//input[@id="user_login"]').inputValue();
-            const actualPassword = await page.locator('//input[@id="user_pass"]').inputValue();
+            const actualUsername = await page.locator(xpathUserName).inputValue();
+            const actualPassword = await page.locator(xpathPassword).inputValue();
+
             expect(actualUsername).toBe(invalidAccount.username);
             expect(actualPassword).toBe(invalidAccount.password);
         });
 
-        await test.step("Click button login", async () => {
+        await test.step('Click button login', async () => {
             //click button
-            await page.locator('//input[@id="wp-submit"]').click();
-            await expect(page.locator('//div[@id="login_error"]')).toContainText(loginErrorMessage);
+            await page.locator(xpathBtnSubmit).click();
+            await expect(page.locator(xpathLoginError)).toContainText(loginErrorMessage);
         });
-
     });
-    test("@AUTH_002: Login success", async ({ page }) => {
-        await test.step("Nhập vào thông tin username, password đúng", async () => {
-            await page.locator('//input[@id="user_login"]').fill(validAccount.username);
-            await page.locator('//input[@id="user_pass"]').fill(validAccount.password);
+
+    test('@AUTH_002: Login success', async ({ page }) => {
+        await test.step('Nhập vào thông tin username, password đúng', async () => {
+            await page.locator(xpathUserName).fill(validAccount.username);
+            await page.locator(xpathPassword).fill(validAccount.password);
 
             //Kiểm tra giá trị của username password đã nhập
-            const actualUsername = await page.locator('//input[@id="user_login"]').inputValue();
-            const actualPassword = await page.locator('//input[@id="user_pass"]').inputValue();
+            const actualUsername = await page.locator(xpathUserName).inputValue();
+            const actualPassword = await page.locator(xpathPassword).inputValue();
             expect(actualUsername).toBe(validAccount.username);
             expect(actualPassword).toBe(validAccount.password);
         });
 
-        await test.step("Click button login", async () => {
+        await test.step('Click button login', async () => {
             //click button
-            await page.locator('//input[@id="wp-submit"]').click();
+            await page.locator(xpathBtnSubmit).click();
             const adminURL = page.url();
             expect(adminURL).toContain('/wp-admin');
+
+            const headingDashboard = page.locator("//h1[text()='Dashboard']");
+            const headingAtAGlance = page.locator("//h2[text()='At a Glance']");
+            const headingActivity = page.locator("//h2[text()='Activity']");
+
+            await expect(headingDashboard).toBeVisible();
+            await expect(headingAtAGlance).toBeVisible();
+            await expect(headingActivity).toBeVisible();
         });
     });
 });
-
-
